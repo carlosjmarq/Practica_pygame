c2b514c33bb4f558981efcd1b0cd0724fc15de6b
Carlos J Marquez <carlosjmarqmail.com>
2022-02-28 16:52:51 -0400

    Firts commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('DragoRunner')
+clock = pygame.time.Clock()
+
+while True:
+	#draw all elements
+	#update everything
+	pygame.display.update()
