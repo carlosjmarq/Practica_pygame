7beca582574507931d57171d6c893f2e0aa0eee7
Carlos J Marquez <carlosjmarqmail.com>
2022-02-28 18:12:10 -0400

    adicion de sprites

--- code/level.py
+++ code/level.py
@@ -1,11 +1,27 @@
 import pygame
+from settings import *
+from player import Player
+from tile import Tile
 
 class Level:
-	def __init__(self) -> None:
+	def __init__(self):
 		self.display_surface = pygame.display.get_surface()
 		self.visible_sprites = pygame.sprite.Group()
 		self.obstacle_sprites = pygame.sprite.Group()
 	
+		self.create_map()
+
 	def run(self):
 		#drawing and updating the game
+		self.visible_sprites.draw(self.display_surface)
 		pass
+
+	def create_map(self):
+		for i,row in enumerate(WORLD_MAP):
+			for j,cell in enumerate(row):
+				x = j * TILESIZE
+				y = i * TILESIZE
+				if cell == 'x':
+					Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
+				if cell == 'p':
+					Player((x,y),[self.visible_sprites])
